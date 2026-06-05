7a2e6f9bd9a9b559f60709d5a4629aead9425408
adnan20124006 <adnan20124006@digitechuniversity.ac.id>
2026-06-05 20:27:23 +0700

    ✨ | Kelompok 3 PML | 20124006 - adnan: README.md

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tugas Machine Learning Kelompok 3',
+    author='Kelompok 3',
+    license='',
+)
